a701eef5337a0759f2a53f7b2b8082d9de6e57f4
GabyD002 <andrei.dobrita@devnest.ro>
2022-11-16 02:51:11 +0200

    php_3

--- ProductsInterface.php
+++ ProductsInterface.php
@@ -1,10 +1,12 @@
 <?php
 
-interface ProductsInterface {
+interface ProductsInterface
+{
+    public function setPrice(int $newPrice): void;
 
-    public function setPrice(int $newPrice):void;
-    public function setName(string $newName):void;
+    public function setName(string $newName): void;
 
-    public function getPrice():int;
-    public function getName():string;
+    public function getPrice(): int;
+
+    public function getName(): string;
 }
